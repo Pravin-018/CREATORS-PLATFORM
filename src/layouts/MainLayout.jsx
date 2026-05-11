a422151ce94a459e9588e392bf12ea4e11a8467a
Sai Pragadesh <saipragadeshmohanraj@gmail.com>
2026-05-11 09:14:10 +0530

    Add frontend foundation with routing and layout

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,25 @@
+import Header from "../components/Header";
+
+import Footer from "../components/Footer";
+
+import { Outlet } from "react-router-dom";
+
+function MainLayout() {
+
+return (
+
+<>
+
+<Header />
+
+<Outlet />
+
+<Footer />
+
+</>
+
+);
+
+}
+
+export default MainLayout;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+function Home() {
+
+return <h1>Welcome to your platform</h1>;
+
+}
+
+export default Home;
